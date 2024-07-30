56483b5818b431f50f33fa04aa5ddf1708fbccdb
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-07-29 22:49:27 -0300

    padrão DTO

--- src/main/java/com/lonfyuri/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/lonfyuri/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lonfyuri.springbootmongodb.resources;
 
 import com.lonfyuri.springbootmongodb.domain.User;
+import com.lonfyuri.springbootmongodb.dto.UserDTO;
 import com.lonfyuri.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> userDTOList = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
     }
 }
