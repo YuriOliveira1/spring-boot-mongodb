d113f01e1b1fa53b7a5a9434573167517c4394a7
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-07-31 17:28:55 -0300

    FindUserById

--- src/main/java/com/lonfyuri/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/lonfyuri/springbootmongodb/resources/UserResource.java
@@ -5,12 +5,8 @@ import com.lonfyuri.springbootmongodb.dto.UserDTO;
 import com.lonfyuri.springbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,5 +22,11 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> userDTOList = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
