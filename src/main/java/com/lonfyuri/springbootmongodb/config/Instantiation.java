300d1b66d5a566e64b2cebbe8abfc13346d13b37
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-07-31 21:30:23 -0300

    Comments in Posts

--- src/main/java/com/lonfyuri/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/lonfyuri/springbootmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.lonfyuri.springbootmongodb.config;
 import com.lonfyuri.springbootmongodb.domain.Post;
 import com.lonfyuri.springbootmongodb.domain.User;
 import com.lonfyuri.springbootmongodb.dto.AuthorDTO;
+import com.lonfyuri.springbootmongodb.dto.CommentDTO;
 import com.lonfyuri.springbootmongodb.repository.PostRepository;
 import com.lonfyuri.springbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,13 +38,20 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("31/07/2024"), "Odeio andar de Busão", "É horrivel ficar 1H 30 em um onibus, lembrando ida e volta então são 3H", new AuthorDTO(alex));
+        Post post2 = new Post(null, sdf.parse("31/07/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(alex));
+
+        CommentDTO c1 = new CommentDTO("Para de ser Fresco vá", sdf.parse("01/08/2024"), new AuthorDTO(maria));
+        CommentDTO c2 = new CommentDTO("É horrivel mesmo, por isso vou de Helicoptero", sdf.parse("01/08/2024"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Fodase?", sdf.parse("01/08/2024"), new AuthorDTO(maria));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
-        maria.getPosts().addAll(Arrays.asList(post1, post2));
-        userRepository.save(maria);
+        alex.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(alex);
 
     }
 }
