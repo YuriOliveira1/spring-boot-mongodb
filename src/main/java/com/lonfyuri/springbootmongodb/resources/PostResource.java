17f590fa4d7044f0b10623d597ec6e527137a7ec
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-08-01 23:29:40 -0300

    Query Methods add

--- src/main/java/com/lonfyuri/springbootmongodb/resources/PostResource.java
+++ src/main/java/com/lonfyuri/springbootmongodb/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.lonfyuri.springbootmongodb.resources;
 
 import com.lonfyuri.springbootmongodb.domain.Post;
+import com.lonfyuri.springbootmongodb.resources.util.URL;
 import com.lonfyuri.springbootmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value ="/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value ="text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
